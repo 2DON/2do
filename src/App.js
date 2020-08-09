cfa63705d1fde9af144f7a3981e901c4d93ad2e2
wesauis <wesleya.isotton@gmail.com>
2020-08-08 22:15:34 -0300

    app-base
    https://medium.com/@kitze/%EF%B8%8F-from-react-to-an-electron-app-ready-for-production-a0468ecb1da3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
 
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edited <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
