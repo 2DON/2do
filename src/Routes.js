4348d393080d51622233f5b4dd254b0ad4e02e92
wesauis <wesleya.isotton@gmail.com>
2020-09-20 19:20:27 -0300

    basic styles

---
+++ src/Routes.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Start from './pages/start/Start'
+
+function Routes() {
+  return <Start />
+}
+
+export default Routes
---
+++ src/pages/start/Start.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Start.scss'
+
+function Start() {
+  return <div className="Start"></div>
+}
+
+export default Start
