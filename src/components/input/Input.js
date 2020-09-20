d857e6026dd1a9bf86e594d610f53af7dba1a17f
wesauis <wesleya.isotton@gmail.com>
2020-09-20 19:20:58 -0300

    input component

---
+++ src/components/input/Input.js
@@ -0,0 +1,76 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import './Input.scss'
+
+function Input({
+  id,
+  placeholder,
+  type,
+  required,
+  pattern,
+  invalid,
+  message,
+  min,
+  max,
+}) {
+  return (
+    <div className={`Input ${invalid ? 'invalid' : ''}`}>
+      <div>
+        <input
+          type={type}
+          name={id}
+          id={id}
+          required={required}
+          pattern={pattern}
+          minLength={min}
+          maxLength={max}
+          placeholder=" "
+        />
+        <label htmlFor={id}>{placeholder}</label>
+        <span>{message}</span>
+      </div>
+    </div>
+  )
+}
+
+Input.propTypes = {
+  id: PropTypes.string.isRequired,
+  placeholder: PropTypes.string.isRequired,
+  type: PropTypes.oneOf([
+    'color',
+    'date',
+    'datetime-local',
+    'email',
+    'month',
+    'number',
+    'password',
+    'radio',
+    'range',
+    'reset',
+    'search',
+    'submit',
+    'tel',
+    'text',
+    'time',
+    'url',
+    'week',
+  ]),
+  required: PropTypes.bool,
+  pattern: PropTypes.string,
+  invalid: PropTypes.bool,
+  message: PropTypes.string,
+  min: PropTypes.number,
+  max: PropTypes.number,
+}
+
+Input.defaultProps = {
+  type: 'text',
+  required: false,
+  pattern: undefined,
+  invalid: false,
+  message: '',
+  min: undefined,
+  max: undefined,
+}
+
+export default Input
