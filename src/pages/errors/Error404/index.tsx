eca8b7ca7170f0c2d4e999e291718e9838c2ca0c
arthursiq5 <arthur.siq5@gmail.com>
2020-11-07 17:05:23 -0300

    generated error404 page

---
+++ src/pages/errors/Error404/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import DefaultError from "../DefaultError";
+
+export default const Error404 = () => {
+    return <DefaultError title="Erro 404">
+    	<p>O item solicitado não existe</p>
+    </DefaultError>
+}
