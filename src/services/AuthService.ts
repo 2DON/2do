cb853d8ec6fd29e2cfa0d88a4465c293cf66090e
wesauis <wesleya.isotton@gmail.com>
2020-11-22 17:53:40 -0300

    update AuthService

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,18 +1,70 @@
-import { AxiosResponse } from 'axios';
 import api from '../api';
 import { TOKEN_HEADER } from '../config';
+import { CONFLICT, OK } from '../utils/Status';
 
 const HEADER = TOKEN_HEADER.toLowerCase();
 
+/**
+ * @throws
+ * 
+ * NOT_FOUND      account not found
+ * 
+ * LOCKED         not verified
+ * 
+ * UNAUTHORIZED   invalid password
+ */
 export async function signIn(email: string, password: string): Promise<string> {
-  const response = await api.post('/accounts/sign-in', { email, password });
+  const { status, headers } = await api.post(
+    '/auth/sign-in', 
+    { email, password });
 
-  return response.headers[HEADER];
+  if (status === OK) {
+    return headers[HEADER];
+  } else {
+    throw status;
+  }
 }
 
-export async function signUp(
-  email: string,
-  password: string
-): Promise<AxiosResponse<void>> {
-  return api.post('/accounts/sign-up', { email, password });
+/**
+ * @throws
+ * 
+ * BAD_REQUEST     invalid request
+ * 
+ * CONFLICT        email already in use
+ */
+export async function signUp(email: string, 
+                             password: string, 
+                             name?: string, 
+                             options?: string): Promise<void> {
+  const { status } = await api.post(
+    '/auth/sign-up', 
+    { email, password, name, options });
+
+  if (status === CONFLICT) {
+    return;
+  } else {
+    throw status;
+  }
+}
+
+/**
+ * @throws
+ * 
+ * NOT_FOUND       account not found
+ * 
+ * UNAUTHORIZED    account is already verified
+ * 
+ * CONFLICT        email already in use
+ */
+export async function signUpFixEmail(email: string, 
+                                     newEmail: string): Promise<void> {
+  const { status } = await api.post(
+    `/auth/sign-up/${encodeURIComponent(email)}`, 
+    { ["new-email"]: newEmail });
+
+  if (status === OK) {
+   return; 
+  } else {
+    throw status;
+  }
 }
---
+++ src/utils/Status.ts
@@ -0,0 +1,15 @@
+export const OK = 200
+
+export const CREATED = 201
+
+export const NOT_FOUND = 404
+
+export const BAD_REQUEST = 400
+
+export const CONFLICT = 409
+
+export const LOCKED = 423
+
+export const UNAUTHORIZED = 401
+
+export const UPGRADE_REQUIRED = 426
